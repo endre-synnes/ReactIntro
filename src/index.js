4ee24e35bfdf71960869a27b541ee47a171b2be3
synend16 <synend16@student.westerdals.no>
2018-09-10 22:07:00 +0200

    now clickable videolist

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import env from "../env/google_api"
 import SearchBar from "./components/search_bar";
 import YTSearch from "youtube-api-search";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 
 const API_KEY = env.env;
@@ -14,10 +15,16 @@ const API_KEY = env.env;
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: "surfboard"},  (videos) => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term: "Ac Milan"},  (videos) => {
+      this.setState({
+        videos : videos,
+        selectedVideo : videos[0]
+      });
     });
   }
 
@@ -25,7 +32,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   }
